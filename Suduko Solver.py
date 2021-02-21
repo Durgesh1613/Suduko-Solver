f67ba1b2eb5bf2aa4b9bc9cc457a510638bf69a3
Durgesh Kavate <66621663+Durgesh1613@users.noreply.github.com>
2021-02-21 14:28:52 +0530

    Add files via upload

---
+++ Suduko Solver.py
@@ -0,0 +1,207 @@
+
+#First Row
+print("First Row")
+fi_fi = int(input("Enter The First Row First Digit:"))
+fi_se = int(input("Enter The First Row Second Digit:"))
+fi_t = int(input("Enter The First Row Third Digit:"))
+fi_f = int(input("Enter The First Row Fourth Digit:"))
+fi_ff = int(input("Enter The First Row Fifth Digit:"))
+fi_s = int(input("Enter The First Row Sixth Digit:"))
+fi_ss = int(input("Enter The First Row Seventh Digit:"))
+fi_e = int(input("Enter The First Row Eigth Digit:"))
+fi_n = int(input("Enter The First Row Nineth Digit:"))
+
+#Second Row
+print(" ")
+print("Second Row")
+s_fi = int(input("Enter The Second Row First Digit:"))
+s_se = int(input("Enter The Second Row Second Digit:"))
+s_t = int(input("Enter The Second Row Third Digit:"))
+s_f = int(input("Enter The Second Row Fourth Digit:"))
+s_ff = int(input("Enter The Second Row Fifth Digit:"))
+s_s = int(input("Enter The Second Row Sixth Digit:"))
+s_ss = int(input("Enter The Second Row Seventh Digit:"))
+s_e = int(input("Enter The Second Row Eigth Digit:"))
+s_n = int(input("Enter The Second Row Nineth Digit:"))
+
+#Third Row
+print(" ")
+print("Third Row")
+t_fi = int(input("Enter The Third Row First Digit:"))
+t_se = int(input("Enter The Third Row Second Digit:"))
+t_t = int(input("Enter The Third Row Third Digit:"))
+t_f = int(input("Enter The Third Row Fourth Digit:"))
+t_ff = int(input("Enter The Third Row Fifth Digit:"))
+t_s = int(input("Enter The Third Row Sixth Digit:"))
+t_ss = int(input("Enter The Third Row Seventh Digit:"))
+t_e = int(input("Enter The Third Row Eigth Digit:"))
+t_n = int(input("Enter The Third Row Nineth Digit:"))
+
+#Fourth Row
+print(" ")
+print("Fourth Row")
+f_fi = int(input("Enter The Fourth Row First Digit:"))
+f_se = int(input("Enter The Fourth Row Second Digit:"))
+f_t = int(input("Enter The Fourth Row Third Digit:"))
+f_f = int(input("Enter The Fourth Row Fourth Digit:"))
+f_ff = int(input("Enter The Fourth Row Fifth Digit:"))
+f_s = int(input("Enter The Fourth Row Sixth Digit:"))
+f_ss = int(input("Enter The Fourth Row Seventh Digit:"))
+f_e = int(input("Enter The Fourth Row Eigth Digit:"))
+f_n = int(input("Enter The Fourth Row Nineth Digit:"))
+
+#Fifth Row
+print(" ")
+print("Fifth Row")
+ff_fi = int(input("Enter The Fifth Row First Digit:"))
+ff_se = int(input("Enter The Fifth Row Second Digit:"))
+ff_t = int(input("Enter The Fifth Row Third Digit:"))
+ff_f = int(input("Enter The Fifth Row Fourth Digit:"))
+ff_ff = int(input("Enter The Fifth Row Fifth Digit:"))
+ff_s = int(input("Enter The Fifth Row Sixth Digit:"))
+ff_ss = int(input("Enter The Fifth Row Seventh Digit:"))
+ff_e = int(input("Enter The Fifth Row Eigth Digit:"))
+ff_n = int(input("Enter The Fifth Row Nineth Digit:"))
+
+#Sixth Row
+print(" ")
+print("Sixth Row")
+si_fi = int(input("Enter The Sixth Row First Digit:"))
+si_se = int(input("Enter The Sixth Row Second Digit:"))
+si_t = int(input("Enter The Sixth Row Third Digit:"))
+si_f = int(input("Enter The Sixth Row Fourth Digit:"))
+si_ff = int(input("Enter The Sixth Row Fifth Digit:"))
+si_s = int(input("Enter The Sixth Row Sixth Digit:"))
+si_ss = int(input("Enter The Sixth Row Seventh Digit:"))
+si_e = int(input("Enter The Sixth Row Eigth Digit:"))
+si_n = int(input("Enter The Sixth Row Nineth Digit:"))
+
+#Seventh Row
+print(" ")
+print("Seventh Row")
+ss_fi = int(input("Enter The Seventh Row First Digit:"))
+ss_se = int(input("Enter The Seventh Row Second Digit:"))
+ss_t = int(input("Enter The Seventh Row Third Digit:"))
+ss_f = int(input("Enter The Seventh Row Fourth Digit:"))
+ss_ff = int(input("Enter The Seventh Row Fifth Digit:"))
+ss_s = int(input("Enter The Seventh Row Sixth Digit:"))
+ss_ss = int(input("Enter The Seventh Row Seventh Digit:"))
+ss_e = int(input("Enter The Seventh Row Eigth Digit:"))
+ss_n = int(input("Enter The Seventh Row Nineth Digit:"))
+
+#Eigth Row
+print(" ")
+print("Eigth Row")
+e_fi = int(input("Enter The Eigth Row First Digit:"))
+e_se = int(input("Enter The Eigth Row Second Digit:"))
+e_t = int(input("Enter The Eigth Row Third Digit:"))
+e_f = int(input("Enter The Eigth Row Fourth Digit:"))
+e_ff = int(input("Enter The Eigth Row Fifth Digit:"))
+e_s = int(input("Enter The Eigth Row Sixth Digit:"))
+e_ss = int(input("Enter The Eigth Row Seventh Digit:"))
+e_e = int(input("Enter The Eigth Row Eigth Digit:"))
+e_n = int(input("Enter The Eigth Row Nineth Digit:"))
+
+#Ninth Row
+print(" ")
+print("Ninth Row")
+n_fi = int(input("Enter The Nineth Row First Digit:"))
+n_se = int(input("Enter The Nineth Row Second Digit:"))
+n_t = int(input("Enter The Nineth Row Third Digit:"))
+n_f = int(input("Enter The Nineth Row Fourth Digit:"))
+n_ff = int(input("Enter The Nineth Row Fifth Digit:"))
+n_s = int(input("Enter The Nineth Row Sixth Digit:"))
+n_ss = int(input("Enter The Nineth Row Seventh Digit:"))
+n_e = int(input("Enter The Nineth Row Eigth Digit:"))
+n_n = int(input("Enter The Nineth Row Nineth Digit:"))
+
+
+board = [
+    [fi_fi, fi_se, fi_t, fi_f, fi_ff, fi_s, fi_ss, fi_e, fi_n],
+    [s_fi, s_se, s_t, s_f, s_ff, s_s, s_ss, s_e, s_n],
+    [t_fi, t_se, t_t, t_f, t_ff, t_s, t_ss, t_e, t_n],
+    [f_fi, f_se, f_t, f_f, f_ff, f_s, f_ss, f_e, f_n],
+    [ff_fi, ff_se, ff_t, ff_f, ff_ff, ff_s, ff_ss, ff_e, f_n],
+    [si_fi, si_se, si_t, si_f, si_ff, si_s, si_ss, si_e, si_n],
+    [ss_fi, ss_se, ss_t, ss_f, ss_ff, ss_s, ss_ss, ss_e, ss_n],
+    [e_fi, e_se, e_t, e_f, e_ff, e_s, e_ss, e_e, e_n],
+    [n_fi, n_se, n_t, n_n, n_ff, n_s, n_ss, n_e, n_n],
+]
+
+
+def solve (bo):
+
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range (1, 10):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
+
+            if solve(bo):
+                return True
+
+            bo[row][col] = 0
+
+    return False
+
+
+
+def valid(bo, num, pos):
+    #Check Row
+    for i in range (len(bo[0])):
+        if bo [pos[0]][i] == num and pos[1] != i:
+            return False
+
+    #Check Column
+    for i in range (len(bo)):
+        if bo [i] [pos[1]] == num and pos[0] != i:
+            return False
+
+    #Check Box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range (box_y * 3, box_y*3 + 3):
+        for j in range (box_x * 3, box_x*3 + 3):
+            if bo[i][j] == num and (i, j) != pos:
+                return False
+
+    return True
+
+
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j]) + " ", end="")
+
+
+def find_empty(bo):
+    for i in range (len(bo)):
+        for j in range (len(bo[0])):
+            if bo[i][j] == 0:
+                return (i, j) #row, column
+
+    return None 
+
+print(" ")
+print("THIS IS THE UN-SOLVED BOARD:")
+print_board(board)
+solve(board)
+print(" ")
+print("THIS IS THE SOLVED BOARD:")
+print_board(board)
+
+
